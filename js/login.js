f83f1426b4b71476877d0fbe9bb021ad8aab02b3
shahalammirza <mshahalam1246@gmail.comm>
2026-03-07 14:49:42 +0600

    design login page and add it functionality

---
+++ js/login.js
@@ -0,0 +1,23 @@
+
+document.getElementById('loginbtn').addEventListener('click', (event)=>{
+    event.preventDefault();
+    // get the user name
+    const userName = document.getElementById('username');
+    const catchUserName = userName.value;
+
+    // get the password
+    const password = document.getElementById('pinNumber')
+    const catchPassword = password.value;
+   
+    if(catchUserName === 'admin' && catchPassword === 'admin123'){
+        alert("Login Successfully");
+        window.location.assign('index.html')
+    }else{
+        alert("Invalid username and password")
+        return
+    }
+    
+})
+
+
+
