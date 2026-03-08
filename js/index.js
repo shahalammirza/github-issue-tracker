dee589cfaa0e59839e1e60a4fe2c0badf3527516
shahalammirza <mshahalam1246@gmail.comm>
2026-03-08 15:26:24 +0600

    added spinner functionality for tabs cards

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
 
-
+// step 5 spinner id
+const spinner = document.getElementById('spinner');
 
 // step 1 active button functionality
 const allBtn = document.getElementById('all_btn')
@@ -9,7 +10,6 @@ const closeBtn = document.getElementById('close_btn')
 // step 2 catch the card container id
 const cardContainer = document.getElementById('cardContainer');
 
-
 // step 3 all cards array
 let allIssues = []
 let openIssues = []
@@ -18,37 +18,55 @@ let cardCount = document.getElementById('card_Count')
 
 // step 1
 const activeBtnFunc = (id)=>{
-    allBtn.classList.remove('text-white', 'bg-[#4A00FF]');
-    openBtn.classList.remove('text-white', 'bg-[#4A00FF]');
-    closeBtn.classList.remove('text-white', 'bg-[#4A00FF]');
+    showLoading();
+    setTimeout(()=>{
+        allBtn.classList.remove('text-white', 'bg-[#4A00FF]');
+        openBtn.classList.remove('text-white', 'bg-[#4A00FF]');
+        closeBtn.classList.remove('text-white', 'bg-[#4A00FF]');
 
-    allBtn.classList.add('border', 'border-[#E4E4E7]');
-    openBtn.classList.add('border', 'border-[#E4E4E7]');
-    closeBtn.classList.add('border', 'border-[#E4E4E7]');
+        allBtn.classList.add('border', 'border-[#E4E4E7]');
+        openBtn.classList.add('border', 'border-[#E4E4E7]');
+        closeBtn.classList.add('border', 'border-[#E4E4E7]');
 
-    const selectBtn = document.getElementById(id);
-    selectBtn.classList.add('text-white', 'bg-[#4A00FF]');
+        const selectBtn = document.getElementById(id);
+        selectBtn.classList.add('text-white', 'bg-[#4A00FF]');
 
-    if(id === "all_btn"){
-        displayAllCards(allIssues)
-    }else if(id === "open_btn"){
-        displayAllCards(openIssues)
-    }
-    else if(id === "close_btn"){
-        displayAllCards(closedIssues)
-    }
+        if(id === "all_btn"){
+            displayAllCards(allIssues)
+        }else if(id === "open_btn"){
+            displayAllCards(openIssues)
+        }
+        else if(id === "close_btn"){
+            displayAllCards(closedIssues)
+        }
+        hideLoading()
+    }, 300)
+
 }
+
+// spinner function
+const showLoading = ()=>{
+    spinner.classList.remove('hidden')
+}
+
+const hideLoading = ()=>{
+    spinner.classList.add('hidden')
+}
+
 
 
 
 // step2 all cards catching fetch
 const loadCards = async()=>{
+    showLoading();
     const res = await fetch('https://phi-lab-server.vercel.app/api/v1/lab/issues');
     const data = await res.json();
     const allCardsItem = data.data;
     displayAllCards(allCardsItem);
 
     allIssues = allCardsItem;
+
+    hideLoading();
 
     openIssuesFunc()
     closedIssuesFunc()
@@ -80,7 +98,7 @@ const displayAllCards = (items)=>{
                         ${item.status === 'open' ? '<img src="assets/Open-Status.png" alt="">': '<img src="assets/close.png" alt="">'}
                         
                     </div>
-                    <p class="w-[80px] h-[24px] rounded-3xl bg-[#FEECEC] uppercase flex justify-center items-center text-[#EF4444]">${item.priority}</p>
+                    <p class="w-[80px] h-[24px] text-[12px] font-medium rounded-3xl bg-[#FEECEC] uppercase flex justify-center items-center text-[#EF4444]">${item.priority}</p>
                 </div>
                 <div class="card_content">
                     <h2 class="text-[14px] text-[#1F2937] font-semibold pt-[12px]">${item.title}</h2>
@@ -124,10 +142,11 @@ const createElementsLabel = (arr)=>{
 // step3 filtering cards with there status and assign in the array
 const openIssuesFunc = ()=>{
     const openIssueItems = allIssues.filter((item) => item.status === "open")
-    openIssues = openIssueItems;  
+    openIssues = openIssueItems; 
 }
 
 const closedIssuesFunc = ()=>{
+
     const closedIssueItems = allIssues.filter((item) => item.status === "closed")
     closedIssues = closedIssueItems;
 }
